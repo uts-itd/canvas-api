a70b8f27517aad54c31df3cfe885e4ec79bb4aa7
Adham Muhseen <adham.muhseen@uts.edu.au>
2021-09-03 14:41:42 +1000

    remove test file

--- test.js
+++
@@ -1,6 +0,0 @@
-const canvas = require('./index')('uts.instructure.com', '9126~EUczqSh46MDwZoJk4HBlfcerfQ2XLU1lyWWDUTlYRcU4Vt5xENlxlFofmyAbYNfJ');
-
-
-canvas.enrollmentTerms.listEnrollmentTerms(1).then(terms => {
-  console.log(terms.enrollment_terms.length);
-});
