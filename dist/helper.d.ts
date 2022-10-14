7d0d8b7ea5d1de579aa5cac102b98e5beb399657
Adham <adham.muhseen@gmail.com>
2022-10-14 19:27:37 +1100

    remove dist

--- dist/helper.d.ts
+++
@@ -1,9 +0,0 @@
-import { Settings } from './settings';
-export declare function getHelper(settings: Settings): Helper;
-export interface Helper {
-    get: (endpoint: string, query?: any, nesting?: string) => Promise<any>;
-    post: (endpoint: string, data?: any) => Promise<any>;
-    delete: (endpoint: string, data?: any) => Promise<any>;
-    put: (endpoint: string, data?: any) => Promise<any>;
-    patch: (endpoint: string, data?: any) => Promise<void>;
-}
--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { Settings } from './settings';
-import { Routes } from './lib/routes';
-export declare function Canvas(domain: string, token: string): Canvas;
-declare type Canvas = {
-    settings: Settings;
-} & Routes;
-export {};
